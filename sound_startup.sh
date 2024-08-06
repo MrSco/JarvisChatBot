75198d0b0aea3760d5a49717a7ea106d21f8ffd3
MrSco <MrSco@users.noreply.github.com>
2024-08-06 13:15:09 -0400

    refactor: Update sound_startup.sh to use absolute file paths

--- sound_startup.sh
+++ sound_startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo alsactl --file ~/.config/asound.state restore 
-aplay -D plughw:CARD=seeed2micvoicec,DEV=0 ~/JarvisChatBot/sounds/winxp_startup.wav
+sudo alsactl --file /home/user/.config/asound.state restore 
+aplay -D plughw:CARD=seeed2micvoicec,DEV=0 /home/user/JarvisChatBot/sounds/winxp_startup.wav
