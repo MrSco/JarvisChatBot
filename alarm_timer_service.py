b6c6f5a30cf993acb12de685c22ab31f6d4bffb4
MrSco <MrSco@users.noreply.github.com>
2024-08-06 22:59:13 -0400

    refactor: Update AlarmTimerService to use systemd timers for alarms and timers instead of cron

--- alarm_timer_service.py
+++ alarm_timer_service.py
@@ -12,21 +12,18 @@ class AlarmTimerService:
     def __init__(self):
         self.cron_file = "/etc/cron.d/alarm_timer_cron"
         self.is_windows = platform.system() == "Windows"
-        self.python_exe = "venv/bin/python3" if not self.is_windows else "venv/Scripts/python"
+        self.python_exe = os.path.join(script_dir, "venv/bin/python3" if not self.is_windows else "venv/Scripts/python")
         
     def add_alarm(self, alarm_time, callback):
         print(f"Setting alarm for {alarm_time}.")
         service_name = 'jarvis_alarm.service'
         timer_name = 'jarvis_alarm.timer'
-        #cron_time = alarm_time.strftime('%M %H %d %m *')
         if self.is_windows:
             self._add_scheduled_task(alarm_time, "JarvisChatBotAlarmTask", "alarm")
         else:
             self._create_systemd_service(service_name, 'alarm')
             self._create_systemd_timer(timer_name, alarm_time, service_name, is_alarm=True)
             self._reload_and_start_timer(timer_name)
-            #cron_command = os.path.join(script_dir, f"{self.python_exe} {trigger_script_path} alarm")
-            #self._add_cron_job(cron_time, cron_command)
 
     def add_timer(self, duration, callback):
         print(f"Setting timer for {duration} seconds.")
@@ -40,16 +37,6 @@ class AlarmTimerService:
             self._create_systemd_service(service_name, 'timer')
             self._create_systemd_timer(timer_name, timer_time, service_name, is_alarm=False)
             self._reload_and_start_timer(timer_name)
-            # cron_command = os.path.join(script_dir, f"{self.python_exe} {trigger_script_path} timer")
-            # self._add_cron_job(cron_time, cron_command)
-
-    # def _add_cron_job(self, cron_time, cron_command):
-    #     with open(self.cron_file, 'a') as cron_file:
-    #         cron_file.write(f"{cron_time} {cron_command}\n")
-    #     cron_command = f'"{cron_command}"'
-    #     full_command = ["crontab", self.cron_file]
-    #     print("Running alarm/timer command: " + " ".join(full_command))
-    #     subprocess.run(full_command)
 
     def _add_scheduled_task(self, run_time, task_name, type):
         today = datetime.now()
@@ -60,7 +47,7 @@ class AlarmTimerService:
         with open(os.path.join(script_dir, "scheduled_task.xml"), "r", encoding="utf-16") as file:
             scheduled_task_xml = file.read()
         scheduled_task_xml = scheduled_task_xml.replace("{{{URI}}}", task_name)
-        scheduled_task_xml = scheduled_task_xml.replace("{{{Command}}}", '"{}"'.format(os.path.join(script_dir,self.python_exe)))
+        scheduled_task_xml = scheduled_task_xml.replace("{{{Command}}}", '"{}"'.format(self.python_exe))
         scheduled_task_xml = scheduled_task_xml.replace("{{{Arguments}}}", '"{}" {}'.format(os.path.join(script_dir,trigger_script_path), type))
         scheduled_task_xml = scheduled_task_xml.replace("{{{StartBoundary}}}", date_str+"T"+run_time_str)
         scheduled_task_xml = scheduled_task_xml.replace("{{{Date}}}", theDate)
@@ -76,12 +63,6 @@ class AlarmTimerService:
             self._delete_all_scheduled_tasks()
         else:
             self.clear_systemd_timers()
-    #       self._delete_all_cron_jobs()
-
-    # def _delete_all_cron_jobs(self):
-    #     if os.path.exists(self.cron_file):
-    #         os.remove(self.cron_file)
-    #     subprocess.run(["crontab", "-r"])
 
     def _delete_all_scheduled_tasks(self):
         command = ["schtasks", "/delete", "/tn", "JarvisChatBotAlarmTask", "/f"]
