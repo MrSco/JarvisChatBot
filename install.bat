f6afcf45eb70cca93711aad596df03e86c8057e7
MrSco <MrSco@users.noreply.github.com>
2025-03-22 19:59:47 -0400

    refactor: Remove Python and pip checks from installation scripts
    - Eliminated the checks for Python 3 and pip in both install.bat and install.sh to streamline the installation process.
    - This change assumes that the user has the necessary dependencies installed, simplifying the setup for environments where Python is already configured.

--- install.bat
+++ install.bat
@@ -3,20 +3,6 @@
 :: install mpv and vlc with chocolatey
 choco install mpv vlc -y
 
-:: Check for Python 3
-python --version > nul 2>&1
-if %errorlevel% neq 0 (
-    echo Python 3 could not be found. Please install Python 3.
-    exit /b 1
-)
-
-:: Check for pip
-pip --version > nul 2>&1
-if %errorlevel% neq 0 (
-    echo pip for Python 3 could not be found. Please install pip.
-    exit /b 1
-)
-
 :: Create a virtual environment
 python -m venv venv
 
