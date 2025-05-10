cbaaf1aea75a1e15a1766a6fe49e41fda565d2f9
MrSco <MrSco@users.noreply.github.com>
2025-05-10 16:43:05 -0400

    fix: Update service management scripts to ensure shairport-sync is started and stopped correctly

--- stop_all_services.sh
+++ stop_all_services.sh
@@ -1,5 +1,5 @@
 # stop all services
-sudo systemctl stop shairport-sync
 sudo systemctl stop jarvischatbot
 sudo systemctl stop 2mic_button
+sudo systemctl stop shairport-sync
 sudo systemctl stop startup_shutdown_sounds
