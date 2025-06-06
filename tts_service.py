0abf6b42bfba329492a76a0d7d5fb71d07b8fde8
MrSco <MrSco@users.noreply.github.com>
2025-06-06 15:40:35 -0400

    fix: Adjust TTS engine selection for specific assistant name
    - Updated the TextToSpeechService to set the TTS engine to 'pyttsx3' when the assistant name is 'Joshua' and the current engine is 'piper'.
    - This change ensures the correct TTS engine is used based on the assistant's identity, improving the overall functionality of the service.

--- tts_service.py
+++ tts_service.py
@@ -27,6 +27,8 @@ class TextToSpeechService:
         self.tts_engine = config.get("tts_engine", "pyttsx3")
         if self.elevenlabs_voice_id == "" and self.tts_engine == "elevenlabs":
             self.tts_engine = "piper"
+        if self.tts_engine == "piper" and self.assistant_name.lower() == "joshua":
+            self.tts_engine = "pyttsx3"
         self.language = config["language"]
         self.accent = config["assistant_dict"]["accent"]
         self.sound_effect = None
